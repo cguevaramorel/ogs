a8ca7eab959fcc67560e808cb7d24422dc331b3f
Dmitry Yu. Naumov <github@naumov.de>
2018-10-30 17:53:11 +0100

    [PL] SDN; Unify ProcessData. Ref temp. rho, g.

--- ProcessLib/SmallDeformationNonlocal/SmallDeformationNonlocalProcessData.h
+++ ProcessLib/SmallDeformationNonlocal/SmallDeformationNonlocalProcessData.h
@@ -1,6 +1,6 @@
 /**
  * \copyright
- * Copyright (c) 2012-2017, OpenGeoSys Community (http://www.opengeosys.org)
+ * Copyright (c) 2012-2018, OpenGeoSys Community (http://www.opengeosys.org)
  *            Distributed under a Modified BSD License.
  *              See accompanying file LICENSE.txt or
  *              http://www.opengeosys.org/project/license
@@ -12,11 +12,16 @@
 #include <memory>
 #include <utility>
 
-namespace MeshLib
-{
-class Element;
-}
+#include <Eigen/Eigen>
 
+namespace MaterialLib
+{
+namespace Solids
+{
+template <int DisplacementDim>
+struct MechanicsBase;
+}
+}  // namespace MaterialLib
 namespace ProcessLib
 {
 namespace SmallDeformationNonlocal
@@ -27,20 +32,16 @@ struct SmallDeformationNonlocalProcessData
     SmallDeformationNonlocalProcessData(
         std::unique_ptr<MaterialLib::Solids::MechanicsBase<DisplacementDim>>&&
             material,
+        double const reference_temperature_,
         double const internal_length_)
         : material{std::move(material)},
+          reference_temperature(reference_temperature_),
           internal_length_squared(internal_length_ * internal_length_)
     {
     }
 
     SmallDeformationNonlocalProcessData(
-        SmallDeformationNonlocalProcessData&& other)
-        : material{std::move(other.material)},
-          dt{other.dt},
-          t{other.t},
-          internal_length_squared{other.internal_length_squared}
-    {
-    }
+        SmallDeformationNonlocalProcessData&& other) = default;
 
     //! Copies are forbidden.
     SmallDeformationNonlocalProcessData(
@@ -54,8 +55,15 @@ struct SmallDeformationNonlocalProcessData
 
     std::unique_ptr<MaterialLib::Solids::MechanicsBase<DisplacementDim>>
         material;
+    /// Solid's density. A scalar quantity, Parameter<double>.
+    Parameter<double> const& solid_density;
+    /// Specific body forces applied to the solid.
+    /// It is usually used to apply gravitational forces.
+    /// A vector of displacement dimension's length.
+    Eigen::Matrix<double, DisplacementDim, 1> const specific_body_force;
     double dt = 0;
     double t = 0;
+    double const reference_temperature;
     double const internal_length_squared;
 
     double injected_volume = 0.0;
